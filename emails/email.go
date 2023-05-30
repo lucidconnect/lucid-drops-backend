beff211f688ca8f4655912a9c959da9bfa9bb386
CreamyMilk <creamym254@gmail.com>
2023-05-30 14:09:24 +0300

    feat : add mailing support

---
+++ emails/email.go
@@ -0,0 +1,92 @@
+package emails
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"text/template"
+
+	"github.com/mailgun/mailgun-go"
+	"github.com/rs/zerolog/log"
+)
+
+type EmailData struct {
+	Subject     string
+	ContentData interface{}
+	EmailTo     string
+	EmailFrom   string
+	Template    string
+}
+
+type EmailResponse struct {
+	MessageID string
+	Response  string
+}
+
+// this function parses variables into the respective templates
+func processTemplate(templatePath string, dynamicData interface{}) (string, error) {
+	var temp bytes.Buffer
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return "", err
+	}
+	tmpl.Execute(&temp, dynamicData)
+	return temp.String(), nil
+}
+
+func SendVerificationEmail(emailAddress, fromEmailAdress, otp, action, expiry string) error {
+
+	templatePath := "emails/templates/emailverification.html"
+	dynamicData := map[string]interface{}{
+		"otp":    otp,
+		"action": action,
+		"expiry": expiry,
+	}
+
+	body, err := processTemplate(templatePath, dynamicData)
+	if err != nil {
+		return err
+	}
+
+	data := EmailData{
+		Subject:     "Verify your email address",
+		ContentData: body,
+		EmailTo:     emailAddress,
+		EmailFrom:   fromEmailAdress,
+		Template:    templatePath,
+	}
+	_, err = SendEmail(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SendEmail(object EmailData) (*EmailResponse, error) {
+	domain := os.Getenv("MAILGUN_DOMAIN")
+	apiKey := os.Getenv("MAILGUN_API_KEY")
+
+	var err error
+	mg := mailgun.NewMailgun(domain, apiKey)
+
+	sender := object.EmailFrom
+	from := fmt.Sprintf("Abacus <%s>", sender)
+	subject := object.Subject
+	recipient := object.EmailTo
+	body := fmt.Sprintf("%v", object.ContentData)
+
+	message := mg.NewMessage(from, subject, body, recipient)
+	message.SetHtml(body)
+
+	response, messageId, err := mg.Send(message)
+	if err != nil {
+		log.Err(err).Msg("Error sending email")
+		return nil, err
+	}
+
+	return &EmailResponse{
+		MessageID: messageId,
+		Response:  response,
+	}, nil
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/agnivade/levenshtein v1.1.1
 	github.com/go-chi/chi v1.5.4
 	github.com/joho/godotenv v1.5.1
+	github.com/mailgun/mailgun-go v2.0.0+incompatible
 	github.com/rs/cors v1.9.0
 	github.com/rs/zerolog v1.29.1
 	github.com/satori/go.uuid v1.2.0
@@ -18,6 +19,10 @@ require (
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/facebookgo/ensure v0.0.0-20200202191622-63f1cf65ac4c // indirect
+	github.com/facebookgo/stack v0.0.0-20160209184415-751773369052 // indirect
+	github.com/facebookgo/subset v0.0.0-20200203212716-c811ad88dec4 // indirect
+	github.com/gobuffalo/envy v1.10.2 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -28,6 +33,10 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/urfave/cli/v2 v2.25.3 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
@@ -35,5 +44,6 @@ require (
 	golang.org/x/mod v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/tools v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
