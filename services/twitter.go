0b6c76f0426168620aa0cd918810f7e8e43833b1
Deji Ajibola <harjhibolar@gmail.com>
2023-06-09 11:16:23 +0100

    chore : refactor tweet details implementation to generate guest tokens

--- services/twitter.go
+++ services/twitter.go
@@ -7,10 +7,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 	"strings"
 
 	"inverse.so/graph/model"
 	"inverse.so/structure"
+	"inverse.so/utils"
 )
 
 func FetchTweetDetails(link string) (*model.TweetDetails, error) {
@@ -20,7 +23,12 @@ func FetchTweetDetails(link string) (*model.TweetDetails, error) {
 		return nil, err
 	}
 
-	tweet, err := fetchTweetFromID(*id)
+	token := getGuestToken(link)
+	if token == nil {
+		return nil, errors.New("could not get guest token")
+	}
+
+	tweet, err := fetchTweetFromID(*id, *token)
 	if err != nil {
 		return nil, err
 	}
@@ -29,15 +37,40 @@ func FetchTweetDetails(link string) (*model.TweetDetails, error) {
 
 		if strings.EqualFold(tweet.EntryID, fmt.Sprintf("tweet-%s", *id)) {
 			return &model.TweetDetails{
-				ProfilePhoto: tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.ProfileImageURLHTTPS,
-				ProfileName: tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.Name,
+				ProfilePhoto:  tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.ProfileImageURLHTTPS,
+				ProfileName:   tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.Name,
 				ProfileHandle: tweet.Content.ItemContent.TweetResults.Result.Core.UserResults.Result.Legacy.ScreenName,
-				TweetText: tweet.Content.ItemContent.TweetResults.Result.Legacy.FullText,
+				TweetText:     tweet.Content.ItemContent.TweetResults.Result.Legacy.FullText,
 			}, nil
 		}
 	}
 
 	return nil, errors.New("tweet not found")
+}
+
+func getGuestToken(url string) *string {
+
+	body, err := fetchPrelimPage(url)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	log.Print(*body)
+
+	return utils.GetStrPtr(extractGuestToken(*body))
+}
+
+func extractGuestToken(body string) string {
+
+	re := regexp.MustCompile(`document\.cookie="gt=(\d+)`)
+	match := re.FindStringSubmatch(body)
+	if len(match) > 1 {
+		figures := match[1]
+		return figures
+	} else {
+		return ""
+	}
+
 }
 
 func StripTweetIDFromLink(link string) (*string, error) {
@@ -51,7 +84,7 @@ func StripTweetIDFromLink(link string) (*string, error) {
 	return &tweetID, nil
 }
 
-func fetchTweetFromID(id string) (*structure.TweetResponse, error) {
+func fetchTweetFromID(id, token string) (*structure.TweetResponse, error) {
 
 	url := "https://twitter.com/i/api/graphql/tPRAv4UnqM9dOgDWggph7Q/TweetDetail?variables=%7B%22focalTweetId%22%3A%22" + id + "%22%2C%22with_rux_injections%22%3Afalse%2C%22includePromotedContent%22%3Atrue%2C%22withCommunity%22%3Atrue%2C%22withQuickPromoteEligibilityTweetFields%22%3Atrue%2C%22withBirdwatchNotes%22%3Afalse%2C%22withVoice%22%3Atrue%2C%22withV2Timeline%22%3Atrue%7D&features=%7B%22rweb_lists_timeline_redesign_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Afalse%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Afalse%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D"
 
@@ -70,7 +103,7 @@ func fetchTweetFromID(id string) (*structure.TweetResponse, error) {
 	req.Header.Add("authorization", "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA")
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
 	req.Header.Add("content-type", "application/json")
-	req.Header.Add("x-guest-token", "1666362692196528129")
+	req.Header.Add("x-guest-token", token)
 	req.Header.Add("x-twitter-active-user", "yes")
 	req.Header.Add("sec-ch-ua-platform", "\"macOS\"")
 	req.Header.Add("Accept", "*/*")
@@ -107,4 +140,51 @@ func fetchTweetFromID(id string) (*structure.TweetResponse, error) {
 	}
 
 	return &resp, nil
+}
+
+func fetchPrelimPage(url string) (*string, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+
+	// Headers
+	req.Header.Add("Host", "twitter.com")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("sec-ch-ua", "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"macOS\"")
+	req.Header.Add("Upgrade-Insecure-Requests", "1")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36")
+	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Add("Sec-Fetch-Site", "none")
+	req.Header.Add("Sec-Fetch-Mode", "navigate")
+	req.Header.Add("Sec-Fetch-User", "?1")
+	req.Header.Add("Sec-Fetch-Dest", "document")
+	req.Header.Add("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
+	req.Header.Add("Cookie", "guest_id=v1%3A168609151061125362; guest_id_ads=v1%3A168609151061125362; guest_id_marketing=v1%3A168609151061125362; personalization_id=\"v1_PjJQ9U+VY5UVOc4BAouPNQ==\"")
+
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+
+	// Fetch Request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Read Response Body
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return nil, err
+	}
+
+	return utils.GetStrPtr(string(respBody)), nil
 }
