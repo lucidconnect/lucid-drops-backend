a4a908293a23866fd72a8520fa98a3deafb4b369
Jotham Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-06-27 16:27:38 +0300

    feat : add deployment hook to collection creation

--- models/collection.go
+++ models/collection.go
@@ -1,27 +1,103 @@
 package models
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
 	"inverse.so/graph/model"
+)
+
+const (
+	inverseNFTDeploymentServerURL = "http://localhost:9090/deploy"
 )
 
 type Collection struct {
 	Base
-	CreatorID   uuid.UUID
-	Name        string
-	Image       string `json:"image"`
-	Thumbnail   string `json:"thumbnail"`
-	Description string `json:"description"`
+	CreatorID       uuid.UUID
+	Name            string
+	Image           string `json:"image"`
+	Thumbnail       string `json:"thumbnail"`
+	Description     string `json:"description"`
+	ContractAddress *string
+	TransactionHash *string
+}
+
+type DeplyomenResponse struct {
+	Type    int    `json:"type"`
+	ChainID int    `json:"chainId"`
+	Nonce   int    `json:"nonce"`
+	To      string `json:"to"`
+	Value   struct {
+		Type string `json:"type"`
+		Hex  string `json:"hex"`
+	} `json:"value"`
+	Data          string `json:"data"`
+	Hash          string `json:"hash"`
+	From          string `json:"from"`
+	Confirmations int    `json:"confirmations"`
+}
+
+func (c *Collection) AfterCreate(tx *gorm.DB) (err error) {
+	go func() {
+		client := &http.Client{}
+
+		collectionData, err := json.Marshal(c)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req, err := http.NewRequest(http.MethodPost, inverseNFTDeploymentServerURL, bytes.NewBuffer(collectionData))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.Header.Add("Content-Type", "application/json")
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var tempDesination DeplyomenResponse
+		err = json.Unmarshal(body, &tempDesination)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		c.TransactionHash = &tempDesination.Hash
+		err = tx.Save(c).Error
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+	}()
+	return nil
 }
 
 func (c *Collection) ToGraphData() *model.Collection {
-	testAddress := "0xdEFdf3C58e416aD22b4b47613A41ce1e6050B13B"
 	return &model.Collection{
 		ID:              c.ID.String(),
 		Name:            c.Name,
 		Description:     c.Description,
 		Image:           c.Image,
 		Thumbnail:       c.Thumbnail,
-		ContractAddress: &testAddress,
+		ContractAddress: c.ContractAddress,
 	}
 }
