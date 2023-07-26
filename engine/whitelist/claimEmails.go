8e2342d57e713d037e50c68090c2bdfae99cd6c2
Jotham Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-07-26 08:00:55 +0300

    feat : update email claiming to general claim flow

--- engine/whitelist/claimEmails.go
+++ engine/whitelist/claimEmails.go
@@ -1,21 +1,14 @@
 package whitelist
 
 import (
-	"bytes"
-	"encoding/hex"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"net/http"
 	"time"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/rs/zerolog/log"
+	"inverse.so/dbutils"
 	"inverse.so/emails"
 	"inverse.so/engine"
 	"inverse.so/graph/model"
-	"inverse.so/magic"
 	"inverse.so/models"
 	"inverse.so/utils"
 )
@@ -148,83 +141,20 @@ func CompleteEmailVerificationForClaim(input *model.CompleteEmailVerificationInp
 		return nil, errors.New("an error when verifying the OTP")
 	}
 
-	return &model.CompleteEmailVerificationResponse{
-		OtpRequestID: input.OtpRequestID,
-	}, nil
-}
+	newMint := models.MintPass{
+		ItemId:                    otpDetails.ItemID.String(),
+		ItemIdOnContract:          otpDetails.ItemIdOnContract,
+		CollectionContractAddress: otpDetails.CollectionContractAddress,
+	}
 
-func GenerateSignatureForClaim(input *model.GenerateClaimSignatureInput) (*model.MintAuthorizationResponse, error) {
-	otpDetails, err := engine.GetEmailOTPRecordByID(input.OtpRequestID)
+	err = dbutils.DB.Create(&newMint).Error
 	if err != nil {
-		return nil, errors.New("email verification attempt could not be found")
+		return nil, err
 	}
 
-	if otpDetails.VerifiedAt == nil {
-		return nil, errors.New("email has not been verified yet")
-	}
+	passId := newMint.ID.String()
 
-	if IsThisAValidEthAddress(input.ClaimingAddress) {
-		return nil, errors.New("the passed in address in not a valid Ethereum address")
-	}
-
-	otpDetails.ClaimingAddress = input.ClaimingAddress
-	otpSaveError := engine.SaveModel(otpDetails)
-	if otpSaveError != nil {
-		log.Info().Msgf("🚨 OTP Model failed to updated in DB %+v", otpDetails)
-		return nil, errors.New("an error when verifying the Claim")
-	}
-
-	go func() {
-		inverseAAServerURL := utils.UseEnvOrDefault("INVERSE_AA_SERVER", "https://inverse-aa.onrender.com")
-
-		client := &http.Client{}
-
-		itemData, err := json.Marshal(map[string]interface{}{
-			"receiptientAddresses": []string{input.ClaimingAddress},
-			"items":                []int64{otpDetails.ItemIdOnContract},
-			"contractAddress":      otpDetails.CollectionContractAddress,
-		})
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		req, err := http.NewRequest(http.MethodPost, inverseAAServerURL+"/sendnfts", bytes.NewBuffer(itemData))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		req.Header.Add("Content-Type", "application/json")
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		defer res.Body.Close()
-	}()
-
-	// TODO add back signature flow
-	dataInPackedFormat := utils.EncodePacked(
-		// utils.EncodeAddress("0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160"),
-		utils.EncodeAddress(input.ClaimingAddress), // Some Addresss
-		utils.EncodeUint256("123"),
-		utils.EncodeBytesString(hex.EncodeToString([]byte("coffee and donuts"))),
-		utils.EncodeUint256("1"),
-	)
-
-	rawData := hexutil.Encode(dataInPackedFormat)
-
-	keccakOfTheMessageInBytes := crypto.Keccak256(dataInPackedFormat)
-
-	signature := magic.SecretlySignThisMessage("\x19Ethereum Signed Message:\n32" + string(keccakOfTheMessageInBytes))
-
-	return &model.MintAuthorizationResponse{
-		PackedData:           rawData,
-		MintingAbi:           "['function mint(address _to) public']",
-		MintingSignature:     signature,
-		SmartContractAddress: "0x34bE7f35132E97915633BC1fc020364EA5134863",
+	return &model.CompleteEmailVerificationResponse{
+		OtpRequestID: passId,
 	}, nil
 }
