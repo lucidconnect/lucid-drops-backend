5f5d61510ef6efc7b1598e1d238b199dfb4fe91e
Jotham Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-07-26 07:55:25 +0300

    feat : update nintpass model

--- models/mintpass.go
+++ models/mintpass.go
@@ -1,7 +1,13 @@
 package models
 
+import "time"
+
 type MintPass struct {
 	Base
 	ItemId        string
 	MinterAddress string
+	UsedAt        *time.Time `gorm:"default:null"`
+
+	ItemIdOnContract          int64
+	CollectionContractAddress string
 }
