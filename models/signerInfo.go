77c8ab128bd59527e9189de6e7e8fd58bf211257
Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-08-04 20:52:19 +0300

    feat : make signer signature optional

--- models/signerInfo.go
+++ models/signerInfo.go
@@ -4,9 +4,9 @@ import "inverse.so/graph/model"
 
 type SignerInfo struct {
 	Base
-	CreatorID     string  `gorm:"type:uuid;index:idx_creator_id,unique;not null;"`
-	WalletAddress string  `gorm:"type:varchar(255);index:idx_wallet_address,unique;not null;"`
-	Signature     *string `gorm:"type:varchar(255);index:idx_signature,unique;not null;"`
+	CreatorID     string `gorm:"type:uuid;index:idx_creator_id,unique;not null;"`
+	WalletAddress string `gorm:"type:varchar(255);index:idx_wallet_address,unique;not null;"`
+	Signature     *string
 	AltPublicKey  string
 	AltPrivateKey string
 	Provider      model.SignerProvider
