f260e3363760f98a3d9a7f26d8f8280eb9d103e1
Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-08-16 13:57:48 +0300

    feat : update the `GetOnboardinProgress` implementation

--- engine/onboarding/onboardingProgress.go
+++ engine/onboarding/onboardingProgress.go
@@ -9,11 +9,13 @@ func GetOnboardinProgress(address string) (*model.OnboardingProgress, error) {
 	cachedCreator, err := engine.GetCreatorByAddress(address)
 	if err != nil {
 		return &model.OnboardingProgress{
+			Creator:                  nil,
 			RegisterdInverseUsername: false,
 		}, nil
 	}
 
 	return &model.OnboardingProgress{
+		Creator:                  cachedCreator.ToGraphData(),
 		RegisterdInverseUsername: cachedCreator.InverseUsername != nil,
 	}, nil
 
