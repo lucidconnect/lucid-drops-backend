b70dfceef6c7050ef7095bd2c5753de154be06d9
Deji Ajibola <harjhibolar@gmail.com>
2023-09-12 22:13:14 +0100

    chore : fix  issue where eoa aa addresses cannot see their claimed items

--- engine/claimers/claimers.go
+++ engine/claimers/claimers.go
@@ -1,11 +1,20 @@
 package claimers
 
 import (
+	"gorm.io/gorm"
+	"inverse.so/dbutils"
 	"inverse.so/engine"
 	"inverse.so/graph/model"
+	"inverse.so/models"
 )
 
 func FetchClaimedItems(address string) ([]*model.Item, error) {
+
+	aaAddress, err := fetchAAAddressFromSignerInfo(address)
+	if err == nil {
+		address = *aaAddress
+	}
+
 	items, err := engine.GetClaimedItemByAddress(address)
 	if err != nil {
 		return nil, err
@@ -18,4 +27,15 @@ func FetchClaimedItems(address string) ([]*model.Item, error) {
 	}
 
 	return mappedItems, nil
+}
+
+func fetchAAAddressFromSignerInfo(address string) (*string, error) {
+
+	var signerInfo models.SignerInfo
+	err := dbutils.DB.Where("wallet_address = ?", address).First(&signerInfo).Error
+	if err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	return &signerInfo.WalletAddress, nil
 }
