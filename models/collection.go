afdff7213b1e5263b041e6fc1456dd028d37e05d
Kinyua <62656400+CreamyMilk@users.noreply.github.com>
2023-09-14 12:48:59 +0300

    fix : resolve AA contract addresses

--- models/collection.go
+++ models/collection.go
@@ -1,17 +1,8 @@
 package models
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-
-	"github.com/rs/zerolog/log"
 	uuid "github.com/satori/go.uuid"
-	"gorm.io/gorm"
 	"inverse.so/graph/model"
-	"inverse.so/utils"
 )
 
 type Collection struct {
@@ -40,61 +31,62 @@ type DeplyomenResponse struct {
 	Confirmations int    `json:"confirmations"`
 }
 
-func (c *Collection) AfterCreate(tx *gorm.DB) (err error) {
-	go func() {
-		inverseAAServerURL := utils.UseEnvOrDefault("INVERSE_AA_SERVER", "https://inverse-aa.onrender.com")
+// We nolonger trigger AA-wallet deployments
+// func (c *Collection) AfterCreate(tx *gorm.DB) (err error) {
+// 	go func() {
+// 		inverseAAServerURL := utils.UseEnvOrDefault("INVERSE_AA_SERVER", "https://inverse-aa.onrender.com")
 
-		client := &http.Client{}
+// 		client := &http.Client{}
 
-		collectionData, err := json.Marshal(c)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		collectionData, err := json.Marshal(c)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		log.Info().Msgf("🪼 Sending Request to AA server at %s and Data : %s", inverseAAServerURL, utils.AsJson(collectionData))
+// 		log.Info().Msgf("🪼 Sending Request to AA server at %s and Data : %s", inverseAAServerURL, utils.AsJson(collectionData))
 
-		req, err := http.NewRequest(http.MethodPost, inverseAAServerURL+"/deploy", bytes.NewBuffer(collectionData))
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		req, err := http.NewRequest(http.MethodPost, inverseAAServerURL+"/deploy", bytes.NewBuffer(collectionData))
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		req.Header.Add("Content-Type", "application/json")
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		req.Header.Add("Content-Type", "application/json")
+// 		res, err := client.Do(req)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		defer res.Body.Close()
+// 		defer res.Body.Close()
 
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		body, err := io.ReadAll(res.Body)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		var tempDesination DeplyomenResponse
-		err = json.Unmarshal(body, &tempDesination)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		var tempDesination DeplyomenResponse
+// 		err = json.Unmarshal(body, &tempDesination)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-		c.TransactionHash = &tempDesination.Hash
-		err = tx.Save(c).Error
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+// 		c.TransactionHash = &tempDesination.Hash
+// 		err = tx.Save(c).Error
+// 		if err != nil {
+// 			fmt.Println(err)
+// 			return
+// 		}
 
-	}()
-	return nil
-}
+// 	}()
+// 	return nil
+// }
 
 func (c *Collection) ToGraphData() *model.Collection {
-	return &model.Collection{
+	mappedCollection := &model.Collection{
 		ID:              c.ID.String(),
 		Name:            c.Name,
 		Description:     c.Description,
@@ -102,4 +94,10 @@ func (c *Collection) ToGraphData() *model.Collection {
 		Thumbnail:       c.Thumbnail,
 		ContractAddress: c.ContractAddress,
 	}
+
+	if c.AAContractAddress != nil {
+		mappedCollection.ContractAddress = c.AAContractAddress
+	}
+
+	return mappedCollection
 }
