8ad64a2f6f0f26053470d4749695358bae978151
Deji Ajibola <harjhibolar@gmail.com>
2023-09-27 09:47:36 +0100

    fix  : remove package causing build failure

--- utils/httpReq.go
+++ utils/httpReq.go
@@ -1,27 +1,27 @@
 package utils
 
-import (
-	"errors"
-	"fmt"
+// import (
+// 	"errors"
+// 	"fmt"
 
-	"github.com/imroc/req/v3"
-	"github.com/rs/zerolog/log"
-)
+// 	"github.com/imroc/req/v3"
+// 	"github.com/rs/zerolog/log"
+// )
 
-func NewHttpPost(url string, requestBody interface{}) (*req.Response, error) {
+// func NewHttpPost(url string, requestBody interface{}) (*req.Response, error) {
 
-	client := req.C().DevMode()
+// 	client := req.C().DevMode()
 
-	resp, err := client.R().
-		Post(url)
-	if err != nil {
-		log.Err(err).Msg("Func: utils.NewHttpPost. Message: " + err.Error())
-	}
+// 	resp, err := client.R().
+// 		Post(url)
+// 	if err != nil {
+// 		log.Err(err).Msg("Func: utils.NewHttpPost. Message: " + err.Error())
+// 	}
 
-	if !resp.IsSuccessState() {
-		fmt.Println("bad response status:", resp.Status)
-		return nil, errors.New("Func: utils.NewHttpPost. Message: " + resp.Status)
-	}
+// 	if !resp.IsSuccessState() {
+// 		fmt.Println("bad response status:", resp.Status)
+// 		return nil, errors.New("Func: utils.NewHttpPost. Message: " + resp.Status)
+// 	}
 
-	return resp, nil
-}
+// 	return resp, nil
+// }
