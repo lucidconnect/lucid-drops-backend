464765ec7d99602381e7fefa2ee46fcc5b75e508
Deji Ajibola <harjhibolar@gmail.com>
2023-11-20 10:23:31 +0000

    chore : add itemID to telegram authdetails model

--- models/telegramAuthDetails.go
+++ models/telegramAuthDetails.go
@@ -2,8 +2,9 @@ package models
 
 type TelegramAuthDetails struct {
 	Base
-	UserID    string `json:"user_id"`
-	Username  string `json:"username"`
-	Hash      string `json:"hash"`
-	PhotoURL  string `json:"photo_url"`
+	UserID   string  `json:"user_id"`
+	Username string  `json:"username"`
+	Hash     string  `json:"hash"`
+	PhotoURL string  `json:"photo_url"`
+	ItemID   *string `gorm:"default:null"`
 }
