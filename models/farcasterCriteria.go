7dfacceee0c168e7ecafac1928b09f99781ec034
olawolu <gbemigaolawolu@gmail.com>
2024-03-13 09:09:24 +0100

    add farcaster id  to criteria

--- models/farcasterCriteria.go
+++ models/farcasterCriteria.go
@@ -12,4 +12,5 @@ type FarcasterCriteria struct {
 	CastUrl      string
 	CriteriaType model.ClaimCriteriaType
 	Interactions string
+	FarcasterID  string // fid of account to do verifications against
 }
