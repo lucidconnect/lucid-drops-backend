a3b3c5c109cd18a525e1ca8d63b697780664a02e
olawolu <gbemigaolawolu@gmail.com>
2024-03-16 13:17:56 +0100

    ...

--- models/farcasterCriteria.go
+++ models/farcasterCriteria.go
@@ -7,7 +7,7 @@ import (
 
 type FarcasterCriteria struct {
 	Base
-	DropID             uuid.UUID `gorm:"unique;index:idx_drop_id;not null"`
+	DropId             uuid.UUID `gorm:"unique;index:idx_drop_id;not null"`
 	CreatorID          uuid.UUID
 	CastUrl            string
 	CriteriaType       model.ClaimCriteriaType
