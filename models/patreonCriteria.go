0a1467369bba409fa3a708ea5926baa0c1fc3f04
olawolu <gbemigaolawolu@gmail.com>
2024-04-02 00:24:57 +0100

    deletes

--- models/patreonCriteria.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type PatreonCriteria struct {
-	Base
-	AuthID       string `gorm:"index"`
-	ItemID       string `gorm:"index"`
-	CreatorID    string `gorm:"index"`
-	CampaignName string `gorm:"index"`
-}
