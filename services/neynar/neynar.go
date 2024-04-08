4b3eaa6d3e0c916778c7e6e23506b3514f28a897
olawolu <gbemigaolawolu@gmail.com>
2024-04-08 15:51:28 +0100

    add tests

--- services/neynar/neynar.go
+++ services/neynar/neynar.go
@@ -360,33 +360,42 @@ func decodeFarcasterUser(response io.ReadCloser, address string) (UserDehydrated
 }
 
 func (nc *NeynarClient) validateFarcasterChannelFollowerCriteria(fid int32, criteria drops.FarcasterCriteria) bool {
-	var followers ChannelFollowers
-	var err error
+	var allFollowers []UserDehydrated
 
 	channels := strings.Split(criteria.ChannelID, ",")
 
+	// retrieve all followers
+	// for {
+	// 	followers, err = nc.RetrieveChannelFollowers(channel, fid, "")
+	// }
+
 	for _, channel := range channels {
-		followers, err = nc.RetrieveChannelFollowers(channel, fid, "")
+		followers, err := nc.RetrieveChannelFollowers(channel, fid, "")
 		if err != nil {
 			log.Err(err).Send()
-			continue
+			break
 			// return false
 		}
+		allFollowers = append(allFollowers, followers.Users...)
 		for {
-			for _, follower := range followers.Users {
-				if follower.Fid == fid {
-					return true
-				}
-			}
 			if followers.Next.Cursor == "" {
-				continue
+				break
 			} else {
-				followers, err = nc.RetrieveChannelFollowers(criteria.ChannelID, fid, followers.Next.Cursor)
+				followers, err = nc.RetrieveChannelFollowers(channel, fid, followers.Next.Cursor)
 				if err != nil {
 					log.Err(err).Send()
 					break
 				}
+				allFollowers = append(allFollowers, followers.Users...)
 			}
+		}
+	}
+	fmt.Println(allFollowers)
+	fmt.Println(len(allFollowers))
+
+	for _, v := range allFollowers {
+		if v.Fid == fid {
+			return true
 		}
 	}
 
